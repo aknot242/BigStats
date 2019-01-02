76040649ce73379339f028f58749c9c7d386861b
Daniel Edgar <aknot242@gmail.com>
2019-01-02 08:56:37 -0800

    casing update

--- build/buildRpm.sh
+++ build/buildRpm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 VERSION=0.6
 RELEASE=0010
-RPM_NAME=bigstats-${VERSION}-${RELEASE}.noarch.rpm
+RPM_NAME=BigStats-${VERSION}-${RELEASE}.noarch.rpm
 rm -rf node_modules
 npm install --production
 rpmbuild -bb --define "main $(pwd)" --define '_topdir %{main}/build/rpmbuild' --define "_version ${VERSION}" --define "_release ${RELEASE}" build/bigstats.spec
